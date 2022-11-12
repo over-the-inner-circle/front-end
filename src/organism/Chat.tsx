60a8e3b9d7b1c653c9ac6a3f5729d4e3fad9ee70
Jaewook Kim <jaewkim@c8r4s5.42seoul.kr>
2022-11-12 16:19:34 +0900

    add chat logic

--- src/organism/Chat.tsx
+++ src/organism/Chat.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 const Chat = () => {
+    const [showChatSidebar, setChatSidebar] = useState(false);
+
     return (
-        <div className="bg-blue-500 col-span-1 row-span-2 col-start-2 row-start-2 h-full w-[450px]" id="side">side</div>
+        <>
+            {
+                showChatSidebar ? (
+                    <div className="bg-blue-500 col-span-1 row-span-2 col-start-2 row-start-2 h-full w-[450px]" id="side">side</div>
+                ) : (
+                    <div></div>
+                )
+            }
+
+        </>
     );
 }
 
--- src/pages/Temp.tsx
+++ src/pages/Temp.tsx
@@ -1,10 +1,13 @@
 import Nav from "@/organism/Nav";
 import Game from "@/organism/Game";
 import Chat from "@/organism/Chat";
+import Friends from "@/organism/Friends";
 
+function
 
 function Temp() {
-	  return (
+
+	return (
 		<div className="w-full h-full relative mx-auto my-0" id="root">
 			<Nav></Nav>
 
@@ -12,6 +15,7 @@ function Temp() {
 			<div className="content-start flex h-full w-full relative pt-[78px]">
 				<Game></Game>
 				<Chat></Chat>
+				<Friends></Friends>
 			{/*	<div className="bg-blue-500 col-span-1 row-span-2 col-start-2 row-start-2 h-full w-[450px]" id="side">side</div>*/}
 			</div>
 
