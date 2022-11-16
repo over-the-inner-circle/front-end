bb2e61bc0021f79669e7e76a667302257b04da73
seushin <seushin@student.42seoul.kr>
2022-11-16 15:14:50 +0900

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import Button from '@/atom/Button';
-
-import React, { useReducer } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Intro from '@/pages/Intro';
 import Login from '@/pages/Login';
 import SignUp from '@/pages/SignUp';
-import Main from "@/pages/Main";
-import Temp from "@/pages/Temp";
+import Main from '@/pages/Main';
+import Temp from '@/pages/Temp';
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App h-full">
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Intro />} />
-          <Route path="/login/:provider" element={ <Login /> }/>
+          <Route path="/login/:provider" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/main" element={<Main />} />
           <Route path="/temp" element={<Temp />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </QueryClientProvider>
   );
 }
 
