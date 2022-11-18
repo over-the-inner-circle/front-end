e8b90fb9ead9f70ad070b1af4207fae0f10b3470
seushin <seushin@student.42seoul.kr>
2022-11-18 17:40:50 +0900

    Chat tmp

--- src/organism/Chat.tsx
+++ src/organism/Chat.tsx
@@ -1,16 +1,142 @@
-import { useState } from "react";
-import ChatingRoom from "@/organism/ChatingRoom";
+import { useState } from 'react';
+import ChatingRoom from '@/organism/ChatingRoom';
+import Spinner from '@/atom/Spinner';
+import { fetcher } from '@/hooks/fetcher';
+import SideBarLayout from '@/molecule/SideBarLayout';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
+export interface Room {
+  room_id: string;
+  room_name: string;
+  room_owner_id: string;
+  room_access: 'public' | 'protected' | 'private';
+  created: Date;
+}
+
+function useChattingRooms() {
+  const result = useQuery({
+    queryKey: ['chat/rooms'],
+    queryFn: async (): Promise<Room[]> => {
+      const res = await fetcher('/chat/rooms/all');
+      if (res.ok) {
+        const data = await res.json();
+        return data.rooms;
+      }
+      return [];
+    },
+  });
+  return result;
+}
 
 const Chat = () => {
-	const [room_id, setRoom_Id] = useState<string | null>(null);
+  const [room_id, setRoom_Id] = useState<string | null>(null);
+  const { isError, isLoading, data: chattingRooms } = useChattingRooms();
+  const mutation = useMutation({
+    mutationFn: async (id: string) => {
+      const res = await fetcher(`/chat/room/${id}/join`, {
+        method: 'POST',
+        body: JSON.stringify({ room_password: '' }),
+      });
 
-	if (room_id === null) {
-		return null;
-	}
-	return (
-		<ChatingRoom title={room_id} setRoom_Id={setRoom_Id} />
-	);
+      if (!res.ok) return;
+
+      const data = await res.json();
+
+      setRoom_Id(data.room_id ?? null);
+    },
+  });
+
+  if (isError) return null;
+
+  if (room_id === null) {
+    return (
+      <SideBarLayout>
+        <AddChatRoom />
+        {isLoading ? <Spinner /> : null}
+        {chattingRooms ? (
+          <ul>
+            {chattingRooms.map((room) => (
+              <li key={room.room_id} className="w-full">
+                <button onClick={() => mutation.mutate(room.room_id)}>
+                  {room.room_name}
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </SideBarLayout>
+    );
+  }
+  return (
+    <SideBarLayout>
+      <ChatingRoom roomId={room_id} setRoom_Id={setRoom_Id} />
+    </SideBarLayout>
+  );
+};
+
+function AddChatRoom() {
+  const addChatRoom = useMutation({
+    mutationFn: (event: React.FormEvent<HTMLFormElement>) => {
+      const f = event.target;
+
+      event.preventDefault();
+      return fetcher('/chat/room', {
+        method: 'POST',
+        body: JSON.stringify({
+          room_name: f.room_name.value,
+          room_access: f.room_access.value,
+          room_password: f.room_password.value,
+        }),
+      });
+    },
+  });
+
+  return (
+    <form
+      className="flex h-fit w-full shrink-0 flex-row items-center border-b border-neutral-400 bg-neutral-800"
+      onSubmit={addChatRoom.mutate}
+    >
+      <button className="px-2" type="submit">
+        +
+      </button>
+      <div className="flex w-min flex-col items-center bg-neutral-800">
+        <input
+          className="w-min border-b-4 border-white bg-inherit"
+          name="room_name"
+          type="text"
+          required
+        />
+        <div className="flex flex-row space-y-3 text-xs">
+          <input
+            className="w-min border-b-4 border-white bg-inherit"
+            name="room_access"
+            value="public"
+            type="radio"
+          />
+          <label htmlFor="public">public</label>
+          <input
+            className="w-min border-b-4 border-white bg-inherit"
+            name="room_access"
+            value="protected"
+            type="radio"
+          />
+          <label htmlFor="protected">protected</label>
+          <input
+            className="w-min border-b-4 border-white bg-inherit"
+            name="room_access"
+            value="private"
+            type="radio"
+          />
+          <label htmlFor="private">private</label>
+        </div>
+        <input
+          className="w-min border-b-4 border-white bg-inherit"
+          name="room_password"
+          type="password"
+        />
+      </div>
+    </form>
+  );
 }
 
 export default Chat;
--- src/organism/ChatingRoom.tsx
+++ src/organism/ChatingRoom.tsx
@@ -1,36 +1,102 @@
-export interface ChatProps {
-	title: string;
-	setRoom_Id: (room_id: string | null) => void;
-}
+import { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
 
 export interface ChatProps {
-	title: string;
+  roomId: string;
+  setRoom_Id: (room_id: string | null) => void;
 }
 
-export default function ChatingRoom({ title, setRoom_Id }: ChatProps) {
-	return (
-		<div
-			className="flex flex-col justify-start items-start
-										font-pixel text-white text-sm bg-neutral-600 border-l border-neutral-400
-										w-80 h-full"
-			>
-			<div className="flex justify-between items-center w-full h-10 px-3 border-b bg-neutral-800 border-inherit">
-				{title}
-				<button onClick={() => setRoom_Id(null)} className="px-1">⬅</button>
-			</div>
-			<div className="flex flex-col justify-end items-start w-full h-full border-b border-inherit">
-				<div className="flex flex-col justify-end items-start w-full h-full border-b border-inherit">
-					<li>
-
-					</li>
-				</div>
-				<div className="flex justify-end items-end h-30">
-					<textarea placeholder="plase input here." className="text-black bg-neutral-300 w-full resize-none h-20 border-none" />
-					<button className="h-full px-3 border-b bg-neutral-800 border-inherit">send</button>
-				</div>
-			</div>
-
-		</div>
-	);
+interface UserInfo {
+  user_id: string;
+  nickname: string;
+  provider: string;
+  third_party_id: string;
+  two_factor_authentication_key: string;
+  two_factor_authentication_type: string;
+  prof_img: string;
+  mmr: number;
+  created: Date;
+  deleted: Date;
 }
 
+interface SubscribeData {
+  sender: UserInfo;
+  payload: string;
+}
+
+interface Message {
+  nickname: string;
+  content: string;
+}
+
+function useChat(roomId: string) {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const token = window.localStorage.getItem('access_token');
+  const socketRef = useRef(io('ws://146.56.153.237:9999', { auth: { token } }));
+
+  useEffect(() => {
+    const socket = socketRef.current;
+
+    const handleSub = (data: SubscribeData) => {
+      setMessages((m) => [
+        ...m,
+        { nickname: data.sender.nickname, content: data.payload },
+      ]);
+    };
+
+    socket.emit('join', { room: roomId });
+
+    socket.on('subscribe', handleSub);
+
+    return () => {
+      socket.off('subscribe', handleSub);
+      socket.disconnect();
+    };
+  }, [roomId]);
+
+  return { messages, socket: socketRef.current };
+}
+
+export default function ChatingRoom({ roomId, setRoom_Id }: ChatProps) {
+  const [content, setContent] = useState('');
+  const { messages, socket } = useChat(roomId);
+
+  return (
+    <div
+      className="flex h-full w-80 flex-col
+										items-start justify-start border-l border-neutral-400 bg-neutral-600 font-pixel
+										text-sm text-white"
+    >
+      <div className="flex h-10 w-full items-center justify-between border-b border-inherit bg-neutral-800 px-3">
+        {roomId}
+        <button onClick={() => setRoom_Id(null)} className="px-1">
+          ⬅
+        </button>
+      </div>
+      <div className="flex h-full w-full flex-col items-start justify-end border-b border-inherit">
+        <div className="flex h-full w-full flex-col items-start justify-end border-b border-inherit">
+          {messages.map((message, idx) => (
+            <li key={idx}>{`${message.nickname}: ${message.content}`}</li>
+          ))}
+        </div>
+        <div className="h-30 flex items-end justify-end">
+          <textarea
+            placeholder="plase input here."
+            className="h-20 w-full resize-none border-none bg-neutral-300 text-black"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <button
+            className="h-full border-b border-inherit bg-neutral-800 px-3"
+            onClick={() => {
+              socket.emit('publish', { room: roomId, payload: content });
+              setContent('');
+            }}
+          >
+            send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
