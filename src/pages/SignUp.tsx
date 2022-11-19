38e205c6a8393d158ae8bb72f398f745f0d203ed
JaemooJung <dbdbwer@naver.com>
2022-11-19 21:42:32 +0900

    [refactor] sign-up page tailwind css refactored

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useRef, useCallback, useEffect } from "react";
 import {useLocation, useNavigate} from "react-router-dom";
+import Button from "@/atom/Button";
+import Spacer from "@/atom/Spacer";
 
 const SignUp = () => {
 
@@ -61,7 +63,7 @@ const SignUp = () => {
     navigate("/");
   };
 
-  const handleGoogle2fa = () => {
+  const handle2fa = () => {
     setIs2faOn(!is2faOn);
     // TODO: 2fa on/off
   }
@@ -77,42 +79,46 @@ const SignUp = () => {
   // }, [newUserInfo]);
 
   return (
-    <div className="flex h-screen bg-true-gray text-white font-pixel">
-      <div className="m-auto justify-center">
-          <div className="flex flex-row justify-center mb-20">
-            <div className="mr-20 stop-dragging">
-              <div className="mb-2"> Profile </div>
-              <div className={`flex h-36 w-32 bg-white justify-center items-center`}>
-                {/*<img className="h-full w-full z-0" src={imgUrl} alt="profile" />*/}
-                <div className="text-3xl text-true-gray hover:text-gray-400 z-50"
-                     onClick={onUploadImageButtonClick}> + </div>
-              </div>
-              <input className="hidden" type="file" accept="image/*" ref={inputRef} onChange={onUploadImage}/>
-            </div>
-            <div>
-              <div className="mb-10">
-                <div className="mb-2 stop-dragging"> Username </div>
-                <input type="text" className="text-true-gray w-48 h-10" onChange={onChangeUsernameInput}/>
-              </div>
-              <div className={"stop-dragging"}>
-                <div className={"mb-2"}>Google 2FA</div>
-                <div className="box-content flex h-6 w-6 border-solid border-4 border-white justify-center items-center hover:border-gray-400"
-                     onClick={handleGoogle2fa}>
-                  {is2faOn ? <div className="box-content h-4 w-4 bg-white "></div> : null}
-                </div>
-              </div>
-            </div>
+    <div className="flex flex-col items-center justify-center w-full h-full bg-neutral-900 font-pixel text-white">
+      <div className="flex flex-row gap-20 mb-16">
+        <div className="flex flex-col stop-dragging">
+        <span className="mb-2"> Profile </span>
+        <div className="flex h-36 w-32 bg-white justify-center items-center flex">
+          {/*<img className="h-full w-full" alt="profile" />*/}
+          <input className="hidden"
+                 type="file"
+                 accept="image/*"
+                 ref={inputRef}
+                 onChange={onUploadImage}/>
+          <span className="text-3xl text-true-gray hover:text-gray-400 z-50"
+                onClick={onUploadImageButtonClick}> +
+          </span>
+        </div>
+      </div>
+        <div className="flex flex-col">
+          <div className="mb-2 stop-dragging"> Username </div>
+          <input className="w-48 h-10 bg-white text-true-gray mb-12"
+                 type="text"
+                 value={nickname}
+                 onChange={onChangeUsernameInput}/>
+          <div className="stop-dragging">
+            <span className="mb-2"> 2FA </span>
+            <div className="box-content flex h-6 w-6 border-solid border-4 border-white justify-center items-center hover:border-gray-400"
+                  onClick={handle2fa}>
+              {is2faOn ? <div className="box-content h-4 w-4 bg-white"></div> : null}
+           </div>
           </div>
-          <div className="flex flex-row gap-20 justify-center stop-dragging">
-            <button className={"bg-true-green-600 text-xs py-3 px-5 hover:bg-green-400"}
-                    onClick={signUpNewUser}>
-              Sign Up
-            </button>
-            <button className={"bg-true-gray-600 text-xs py-3 px-6 hover:bg-gray-400"}
-                    onClick={cancelSignUp}>
-              Cancel
-            </button>
-          </div>
+        </div>
+      </div>
+      <div className="flex flex-row gap-20 stop-dragging">
+        <Button className={"bg-true-gray-600 text-xs py-3 px-5 hover:bg-neutral-400"}
+                onClick={cancelSignUp}>
+          Cancel
+        </Button>
+        <Button className={"bg-true-green-600 text-xs py-3 px-5 hover:bg-green-400"}
+                onClick={signUpNewUser}>
+          Sign Up
+        </Button>
       </div>
     </div>
   );
