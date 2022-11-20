75ed3edabc1159af883964e08126a26b1eb3cca6
JaemooJung <dbdbwer@naver.com>
2022-11-20 13:27:11 +0900

    [feat] now image is uploaded and default profile image is added

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,26 +3,47 @@ import {useLocation, useNavigate} from "react-router-dom";
 import Button from "@/atom/Button";
 import Spacer from "@/atom/Spacer";
 
+interface ImageInfo {
+  file: File;
+  url: string;
+  type: string;
+}
+
 const SignUp = () => {
 
   const REQUEST_URL = import.meta.env.VITE_REQUEST_URL;
+  const DEFAULT_PROFILE_IMAGE_URL = '/src/assets/default_profile_image.png';
 
   const [is2faOn, setIs2faOn] = useState(false);
   const [nickname, setNickname] = useState("");
-  // const [imgUrl, setImgUrl] = useState("");
+  const [imageInfo, setImageInfo] = useState<ImageInfo | null>(null);
 
   const navigate = useNavigate();
   const location = useLocation();
   const newUserInfo = location.state;
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const onUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) {
-      return;
+  // const onUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (!e.target.files) {
+  //     return;
+  //   }
+  //   console.log(e.target.value);
+  //   // setImgUrl(e.target.value);
+  // }, []);
+
+  const onUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
+
+    if (fileList && fileList[0]) {
+      const fileUrl = URL.createObjectURL(fileList[0]);
+      setImageInfo({
+        file: fileList[0],
+        url: fileUrl,
+        type: fileList[0].type
+      })
     }
-    console.log(e.target.value);
-    // setImgUrl(e.target.value);
-  }, []);
+    console.log(imageInfo);
+  }
 
   const onUploadImageButtonClick = useCallback(() => {
     if (!inputRef.current) {
@@ -30,6 +51,16 @@ const SignUp = () => {
     }
     inputRef.current.click();
   }, []);
+
+  const currentProfileImageUrl = () => {
+    if (imageInfo) {
+      return imageInfo.url;
+    } else if (newUserInfo && newUserInfo.profImg) {
+      return newUserInfo.profImg;
+    } else {
+      return DEFAULT_PROFILE_IMAGE_URL;
+    }
+  }
 
   const signUpNewUser = async () => {
     // TODO : 회원가입 API POST 요청
@@ -72,38 +103,37 @@ const SignUp = () => {
     setNickname(e.target.value);
   }
 
-  // useEffect(() => {
-  //   if (newUserInfo.profImg !== null) {
-  //     setImgUrl(newUserInfo.profImg);
-  //   }
-  // }, [newUserInfo]);
-
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full bg-neutral-900 font-pixel text-white">
+    <div className="flex flex-col items-center justify-center w-full h-full bg-neutral-900 font-pixel text-white z-0">
       <div className="flex flex-row gap-20 mb-16">
         <div className="flex flex-col stop-dragging">
         <span className="mb-2"> Profile </span>
-        <div className="flex h-36 w-32 bg-white justify-center items-center flex">
-          {/*<img className="h-full w-full" alt="profile" />*/}
+        <div className={`h-36 w-32 bg-cover bg-white`}>
+          <img src={currentProfileImageUrl()}
+               alt="profileImage"
+               className="h-36 w-32 hover:border-gray-400"
+               onClick={onUploadImageButtonClick}
+          />
+          {/*<div className="text-neutral-200 text-stroke text-3xl hover:text-neutral-400 z-50"*/}
+          {/*     onClick={onUploadImageButtonClick}> +*/}
+          {/*</div>*/}
           <input className="hidden"
                  type="file"
-                 accept="image/*"
+                 accept="image/jpeg, image/png, image/jpg"
                  ref={inputRef}
                  onChange={onUploadImage}/>
-          <span className="text-3xl text-true-gray hover:text-gray-400 z-50"
-                onClick={onUploadImageButtonClick}> +
-          </span>
         </div>
       </div>
         <div className="flex flex-col">
           <div className="mb-2 stop-dragging"> Username </div>
-          <input className="w-48 h-10 bg-white text-true-gray mb-12"
+          <input className="w-48 h-10 bg-white text-true-gray mb-10"
                  type="text"
                  value={nickname}
                  onChange={onChangeUsernameInput}/>
           <div className="stop-dragging">
-            <span className="mb-2"> 2FA </span>
-            <div className="box-content flex h-6 w-6 border-solid border-4 border-white justify-center items-center hover:border-gray-400"
+            <span> 2FactorAuth </span>
+            <div className="box-content flex h-6 w-6 border-solid border-4 border-white
+                            justify-center items-center hover:border-gray-400 mt-2"
                   onClick={handle2fa}>
               {is2faOn ? <div className="box-content h-4 w-4 bg-white"></div> : null}
            </div>
