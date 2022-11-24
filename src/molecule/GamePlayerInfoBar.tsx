69f705a37c6e659fad40a6119b915d5de9f88452
JaemooJung <dbdbwer@naver.com>
2022-11-24 17:35:08 +0900

    [feat] now user profile image is diplayed

--- src/molecule/GamePlayerInfoBar.tsx
+++ src/molecule/GamePlayerInfoBar.tsx
@@ -11,6 +11,9 @@ const GamePlayerInfoBar = () => {
 	const gameScore = useRecoilValue(currentGameScore);
 	const playerInfo = useRecoilValue(matchInfo);
 
+	const lPlayerProfileImg = playerInfo?.lPlayerInfo.prof_img;
+	const rPlayerProfileImg = playerInfo?.rPlayerInfo.prof_img;
+
 	if (!playerInfo) {
 		return null;
 	}
@@ -18,7 +21,10 @@ const GamePlayerInfoBar = () => {
 		<>
 		<div className="flex flex-row bg-neutral-800 font-pixel text-white">
 			<div className="flex my-6 ml-6">
-				<Circle radius={32.5} className="fill-yellow-300" />
+				<img className={`shrink-0 w-14 h-14 rounded-full`}
+						 src={lPlayerProfileImg ? lPlayerProfileImg : "src/assets/default_profile_image.png"}
+						 alt={"user profile"}
+				/>
 				<div className="flex flex-col ml-4 items-start justify-center">
 				<span>{playerInfo.lPlayerInfo.nickname}</span>
 				<span>{playerInfo.lPlayerInfo.mmr}</span>
@@ -29,12 +35,15 @@ const GamePlayerInfoBar = () => {
 				{gameScore ? gameScore.p1Score : 0}   :   {gameScore ? gameScore.p2Score : 0}
 			</div>
 			<Spacer />
-			<div className="flex my-6 mr-6">
+			<div className="flex my-6 mr-6 justify-center items-center">
 				<div className="flex flex-col mr-4 items-end justify-center">
 				<span>{playerInfo.rPlayerInfo.nickname}</span>
 				<span>{playerInfo.rPlayerInfo.mmr}</span>
 				</div>
-				<Circle radius={32.5} className="fill-yellow-300" />
+				<img className={`shrink-0 w-14 h-14 rounded-full`}
+						 src={rPlayerProfileImg ? rPlayerProfileImg : "src/assets/default_profile_image.png"}
+						 alt={"user profile"}
+				/>
 			</div>
 		</div>
 		<div className="bg-white w-full h-4"></div>
