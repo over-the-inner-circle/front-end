78ac47b3715ab997c46a5cc3cb9c62f7457dc96e
seushin <seushin@student.42seoul.kr>
2022-11-24 15:22:19 +0900

    Lint

--- src/organism/Nav.tsx
+++ src/organism/Nav.tsx
@@ -1,38 +1,39 @@
-import DmIcon from "@/atom/DmIcon";
-import ChatIcon from "@/atom/ChatIcon";
-import FriendIcon from "@/atom/FriendIcon";
-import { SidebarItem } from "@/pages/Temp";
+import DmIcon from '@/atom/DmIcon';
+import ChatIcon from '@/atom/ChatIcon';
+import FriendIcon from '@/atom/FriendIcon';
+import { SidebarItem } from '@/pages/Temp';
 
 interface NavProps {
-    current: string;
-    onChange(n: SidebarItem): void;
+  current: string;
+  onChange(n: SidebarItem): void;
 }
 
 const Nav = ({ current, onChange }: NavProps) => {
-    return (
-        <div className="flex w-full h-[78px] bg-background/[0.87] justify-between items-center" id="nav">
-            navtext
-          <div className="flex items-center gap-2">
-            <div className="">
-              <button onClick={() => onChange('chat')}>
-                <ChatIcon isActive={current === 'chat'}/>
-              </button>
-            </div>
-            <div className="">
-              <button onClick={() => onChange('friend')}>
-                <FriendIcon isActive={current === 'friend'}/>
-              </button>
-            </div>
-            <div className="">
-              <button onClick={() => onChange('dm')}>
-                <DmIcon isActive={current === 'dm'}/>
-              </button>
-            </div>
-          </div>
-
-
+  return (
+    <div
+      className="bg-background/[0.87] flex h-[78px] w-full items-center justify-between"
+      id="nav"
+    >
+      navtext
+      <div className="flex items-center gap-2">
+        <div className="">
+          <button onClick={() => onChange('chat')}>
+            <ChatIcon isActive={current === 'chat'} />
+          </button>
         </div>
-    )
-}
+        <div className="">
+          <button onClick={() => onChange('friend')}>
+            <FriendIcon isActive={current === 'friend'} />
+          </button>
+        </div>
+        <div className="">
+          <button onClick={() => onChange('dm')}>
+            <DmIcon isActive={current === 'dm'} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
