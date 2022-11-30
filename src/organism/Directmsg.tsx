d227b7e31152c41ad0af5ce5ed4c8ad50ce9ca51
seushin <74958130+seushin@users.noreply.github.com>
2022-11-30 15:27:45 +0900

    [WIP] Feat dm (#51)
    * Extract SideBarHeader
    
    * Extract reusable hooks
    
    * WIP DM

--- src/organism/Directmsg.tsx
+++ src/organism/Directmsg.tsx
@@ -1,11 +1,95 @@
-import SideBarLayout from "@/molecule/SideBarLayout";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { fetcher } from '@/hooks/fetcher';
+import { currentDMOpponentState } from '@/states/currentDMOpponent';
+import SideBarHeader from '@/molecule/SideBarHeader';
+import SideBarLayout from '@/molecule/SideBarLayout';
+import DirectmsgRoom from './DirectmsgRoom';
+import { Friend } from '@/hooks/friends';
 
 const Directmsg = () => {
-	return (
+  const [currentDMOpponent, setCurrentDMOpponent] = useRecoilState(
+    currentDMOpponentState,
+  );
+
+  return (
     <SideBarLayout>
-      directmsg
+      {currentDMOpponent ? (
+        <DirectmsgRoom
+          opponent={currentDMOpponent}
+          close={() => setCurrentDMOpponent(null)}
+        />
+      ) : (
+        <DirectmsgList />
+      )}
     </SideBarLayout>
-	)
+  );
+};
+
+function useSearchUser(nickname: string) {
+  const setCurrentDMOpponent = useSetRecoilState(currentDMOpponentState);
+
+  const data = useQuery<Friend>({
+    queryKey: ['user', nickname],
+    queryFn: async () => {
+      const res = await fetcher(`/user/${nickname}`);
+      if (res.ok) return res.json();
+      throw res;
+    },
+    onSuccess: () => {
+      setCurrentDMOpponent(nickname);
+    },
+    enabled: !!nickname,
+    retry: false,
+  });
+  return data;
+}
+
+function DirectmsgList() {
+  const [nickname, setNickname] = useState('');
+  const [query, setQuery] = useState('');
+  const { isFetching, isError } = useSearchUser(query);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (nickname) {
+      setQuery(nickname);
+    }
+  };
+
+  return (
+    <>
+      <SideBarHeader>Direct Message</SideBarHeader>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full flex-row items-center gap-2 p-3"
+      >
+        <label htmlFor="nickname">To.</label>
+        <input
+          className="w-full bg-neutral-700 p-1 disabled:opacity-20"
+          name="nickname"
+          type="search"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          disabled={isFetching}
+          required
+        />
+        <button
+          className="disabled:opacity-20"
+          type="submit"
+          disabled={isFetching}
+        >
+          Q
+        </button>
+      </form>
+      {isError ? (
+        <div className="flex w-full items-center justify-center text-xs text-red-500">
+          <p>Not Found</p>
+        </div>
+      ) : null}
+    </>
+  );
 }
 
 export default Directmsg;
--- src/organism/DirectmsgRoom.tsx
+++ src/organism/DirectmsgRoom.tsx
@@ -1,45 +1,31 @@
 import { useEffect, useState } from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { fetcher } from '@/hooks/fetcher';
-import {RoomInfo} from "@/states/roomInfoState";
 import { useAutoScroll, useSocketRef } from '@/hooks/chat';
+import { fetcher } from '@/hooks/fetcher';
+import { type Friend } from '@/hooks/friends';
+import StatusIndicator from '@/molecule/StatusIndicator';
 
-export interface ChatProps {
-  roomInfo: RoomInfo;
+interface DirectmsgRoomProps {
+  opponent: string;
   close(): void;
 }
 
-interface UserInfo {
-  user_id: string;
-  nickname: string;
-  provider: string;
-  third_party_id: string;
-  two_factor_authentication_key: string;
-  two_factor_authentication_type: string;
-  prof_img: string;
-  mmr: number;
+interface Message {
+  sender: Friend;
+  payload: string;
   created: Date;
-  deleted: Date;
 }
 
 interface SubscribeData {
-  sender: UserInfo;
+  sender: Friend;
   payload: string;
 }
 
-interface Message {
-  room_msg_id: number;
-  room_id: string;
-  sender: UserInfo;
-  payload: string;
-  created: Date;
-}
-
-function useChatMessages(roomId: string) {
+function useDirectMessages(opponent: string) {
   const data = useQuery<Message[]>({
-    queryKey: ['chat/room/messages', roomId],
+    queryKey: ['dm/messages', opponent],
     queryFn: async () => {
-      const res = await fetcher(`/chat/room/${roomId}/messages`);
+      const res = await fetcher(`/dm/${opponent}/messages`);
 
       if (res.ok) {
         const data = await res.json();
@@ -53,24 +39,19 @@ function useChatMessages(roomId: string) {
   return data;
 }
 
-function useChat(roomId: string) {
+function useDirectMessage(opponent: string) {
   const queryClient = useQueryClient();
   const socketRef = useSocketRef(`ws://${import.meta.env.VITE_BASE_URL}:9999`);
-  const { data: messages } = useChatMessages(roomId);
+  const { data: messages } = useDirectMessages(opponent);
 
   useEffect(() => {
     const socket = socketRef.current;
 
     const handleSub = (data: SubscribeData) => {
       queryClient.setQueryData<Message[]>(
-        ['chat/room/messages', roomId],
+        ['dm/messages', opponent],
         (prevMsg) => {
           const newMessage: Message = {
-            room_msg_id:
-              prevMsg && prevMsg.length
-                ? prevMsg[prevMsg.length - 1].room_msg_id + 1
-                : 1,
-            room_id: roomId,
             sender: data.sender,
             payload: data.payload,
             created: new Date(),
@@ -80,11 +61,11 @@ function useChat(roomId: string) {
       );
     };
 
-    const handleAnnounce = (data) => {
+    const handleAnnounce = (data: unknown) => {
       console.log(data);
     };
 
-    socket.emit('join', { room: roomId });
+    socket.emit('join', { opponent });
 
     socket.on('subscribe', handleSub);
     socket.on('subscribe_self', handleSub);
@@ -95,20 +76,33 @@ function useChat(roomId: string) {
       socket.off('subscribe_self', handleSub);
       socket.off('announcement', handleAnnounce);
     };
-  }, [roomId, queryClient, socketRef]);
+  }, [opponent, queryClient, socketRef]);
 
   return { messages, socket: socketRef.current };
 }
 
-export default function ChatingRoom({ roomInfo, close }: ChatProps) {
+function useOpponent(nickname: string) {
+  const data = useQuery<Friend>({
+    queryKey: ['user', nickname],
+    queryFn: async () => {
+      const res = await fetcher(`/user/${nickname}`);
+      if (res.ok) return res.json();
+      throw res;
+    },
+  });
+  return data;
+}
+
+function DirectmsgRoom({ opponent, close }: DirectmsgRoomProps) {
   const [content, setContent] = useState('');
-  const { messages, socket } = useChat(roomInfo.room_id);
+  const { messages, socket } = useDirectMessage(opponent);
+  const { data: opponentInfo } = useOpponent(opponent);
   const autoScrollRef = useAutoScroll(messages);
 
   const sendMessage = () => {
     const msg = content.trim();
     if (msg) {
-      socket.emit('publish', { room: roomInfo.room_id, payload: msg });
+      socket.emit('publish', { opponent, payload: msg });
     }
     setContent('');
   };
@@ -116,7 +110,29 @@ export default function ChatingRoom({ roomInfo, close }: ChatProps) {
   return (
     <>
       <div className="flex h-fit w-full items-center justify-between border-b border-inherit bg-neutral-800 p-2">
-        {roomInfo.room_name}
+        {opponentInfo ? (
+          <div className="flex flex-row p-2 gap-2">
+            <div className="m-1 flex h-10 w-10 items-center justify-center overflow-hidden rounded-full">
+              <img
+                src={opponentInfo.prof_img}
+                alt="profile"
+                className="h-full w-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col">
+              <p>{opponentInfo.nickname}</p>
+              <div className="flex flex-row items-center space-x-2">
+                <StatusIndicator status={opponentInfo.status} />
+                <p className="min-w-0 truncate text-xs">
+                  {opponentInfo.status ?? 'unknown'}
+                </p>
+              </div>
+            </div>
+              <button className='px-3'>:</button>
+          </div>
+        ) : (
+          opponent
+        )}
         <button onClick={close} className="px-1">
           ⬅
         </button>
@@ -126,9 +142,9 @@ export default function ChatingRoom({ roomInfo, close }: ChatProps) {
         className="h-full w-full grow overflow-y-auto border-b border-inherit"
       >
         <ul className="flex h-fit w-full flex-col items-start justify-start">
-          {messages?.map((message) => (
+          {messages?.map((message, idx) => (
             <li
-              key={message.room_msg_id}
+              key={idx}
               className="h-fit w-full break-words p-1 px-5 text-xs"
             >{`${message.sender.nickname}: ${message.payload}`}</li>
           ))}
@@ -158,3 +174,5 @@ export default function ChatingRoom({ roomInfo, close }: ChatProps) {
     </>
   );
 }
+
+export default DirectmsgRoom;
