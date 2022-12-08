c5f2f1330a19f9a7ceab6d248581d0a9d40bac89
seushin <74958130+seushin@users.noreply.github.com>
2022-12-09 00:24:18 +0900

    Feat password form to enter chat (#85)

--- src/hooks/chat.ts
+++ src/hooks/chat.ts
@@ -1,10 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { io } from 'socket.io-client';
 import { accessTokenState } from '@/states/user/auth';
 import { RoomInfo, roomInfoState } from '@/states/roomInfoState';
 import { useFetcher } from '@/hooks/fetcher';
+import {
+  useDismiss,
+  useFloating,
+  useInteractions,
+} from '@floating-ui/react-dom-interactions';
+import { toast } from 'react-toastify';
 
 export type RoomListType = 'all' | 'joined';
 
@@ -45,19 +51,43 @@ export function useJoinRoom() {
   const fetcher = useFetcher();
   const setRoomInfo = useSetRecoilState(roomInfoState);
   const mutation = useMutation({
-    mutationFn: async (room: RoomInfo) => {
+    mutationFn: async ({
+      room,
+      password = '',
+    }: {
+      room: RoomInfo;
+      password?: string;
+    }) => {
       return fetcher(`/chat/room/${room.room_id}/join`, {
         method: 'POST',
-        body: JSON.stringify({ room_password: '' }),
+        body: JSON.stringify({ room_password: password }),
       });
     },
-    onSuccess: (data, variables) => {
+    onSuccess: (data, info) => {
       if (data.ok) {
-        setRoomInfo(variables ?? null);
+        setRoomInfo(info.room ?? null);
+      } else {
+        throw data;
       }
+    },
+    onError: () => {
+      toast.error('Fail to join this room');
     },
   });
   return mutation;
+}
+
+export function usePasswordForm() {
+  const [open, setOpen] = useState(false);
+  const { floating, context } = useFloating({
+    open,
+    onOpenChange: setOpen,
+  });
+
+  const dismiss = useDismiss(context);
+  const { getFloatingProps } = useInteractions([dismiss]);
+
+  return { open, setOpen, floating, context, getFloatingProps };
 }
 
 export function useSocketRef(url: string) {
