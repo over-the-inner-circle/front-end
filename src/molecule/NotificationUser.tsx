4afb959773d54d2fe7058d57317231692624f07e
seushin <seushin@student.42seoul.kr>
2022-12-09 17:28:23 +0900

    Add notification-user

---
+++ src/molecule/NotificationUser.tsx
@@ -0,0 +1,36 @@
+import type { ToastContentProps } from 'react-toastify';
+import { Friend, useFriendRequestAccept } from '@/hooks/friends';
+import Button from '@/atom/Button';
+
+export interface NotificationUserData {
+  sender: Friend;
+  payload: string;
+}
+
+function NotificationUser({
+  data,
+  closeToast,
+}: ToastContentProps<NotificationUserData>) {
+  const acceptFriendRequest = useFriendRequestAccept();
+
+  const handleClick = () => {
+    if (data?.sender) {
+      acceptFriendRequest.mutate(parseInt(data.payload, 10));
+    }
+    if (closeToast) closeToast();
+  };
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-col items-start justify-start whitespace-nowrap pr-3">
+        <h2 className="text-sm">Friend request</h2>
+        <p>{data?.sender.nickname}</p>
+      </div>
+      <Button className="break-keep bg-green-500 text-xs" onClick={handleClick}>
+        Accept
+      </Button>
+    </div>
+  );
+}
+
+export default NotificationUser;
