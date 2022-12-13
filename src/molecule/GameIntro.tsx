cc7b65ba174f701c3790a6ef7c23a0f67d0bf813
JaemooJung <dbdbwer@naver.com>
2022-12-13 20:48:50 +0900

    [fix] now game start button can be clicked one in a second

--- src/molecule/GameIntro.tsx
+++ src/molecule/GameIntro.tsx
@@ -1,5 +1,5 @@
 import { useSetRecoilState } from 'recoil';
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
 
 import Button from '@/atom/Button';
 import { currentGameStatus } from '@/states/game/currentGameStatus';
@@ -8,6 +8,7 @@ import { GameSocketManager } from '@/models/GameSocketManager';
 const GameIntro = () => {
   const setGameStatus = useSetRecoilState(currentGameStatus);
   const socketManager = GameSocketManager.getInstance();
+  const [isButtonClicked, setIsButtonClicked] = useState<boolean>(false);
 
   useEffect(() => {
     return () => {
@@ -25,14 +26,17 @@ const GameIntro = () => {
       socket.connect();
     }
     //TODO: 여러번 눌렸을 때 어떻게 되는지 체크하기
-    console.log(socket.id);
-    socket.emit('user_join_queue');
-    console.log('user_join_queue emitted');
-    socket.once('user_is_in_queue', () => {
-      console.log('user_is_in_queue received');
+    if (!isButtonClicked) {
       console.log(socket.id);
-      setGameStatus('ON_MATCHING');
-    });
+      socket.emit('user_join_queue');
+      console.log('user_join_queue emitted');
+      socket.once('user_is_in_queue', () => {
+        console.log('user_is_in_queue received');
+        console.log(socket.id);
+        setGameStatus('ON_MATCHING');
+      });
+    }
+    setIsButtonClicked(true);
   };
 
   return (
